ff168eef40e76c8e57ab885355468901e488dfa5
Arijit Banerjee <arijit91@gmail.com>
2013-11-24 03:41:29 -0800

    Compile error fix

--- hash-test/HashTest.cpp
+++ hash-test/HashTest.cpp
@@ -1,19 +1,24 @@
 #include "Snap.h"
 
 int main() {
-  THashPar<TInt, TInt> T;
+  srand(time(0));
+  THashPar<TInt, TIntV> T;
   T.ResizePar(1000);
 
-  #pragma omp parallel for num_threads(1)
-  for (int i = 1; i <= 1000; i++) {
-    //printf("%d\n", i);
-    for (int j = 1; j <= 10000; j++) {
-      T.AddDatPar(rand()%10, rand()%100);
-    }
+  #pragma omp parallel for num_threads(2)
+  for (int i = 1; i <= 1000000; i++) {
+    T.AddDatPar(rand()%100, rand()%100);
   }
 
-  for (THash<TInt, TInt>::TIter it = T.BegI(); it != T.EndI(); it++) {
-    printf("%d %d\n", it.GetKey().Val, it.GetDat().Val);
+  TInt sum = 0;
+  for (THashPar<TInt, TIntV>::TIter it = T.BegI(); it != T.EndI(); it++) {
+    TIntV V = it.GetDat();
+    //printf("%d: ", it.GetKey().Val);
+    sum += V.Len();
+    //for (TInt i = 0; i < V.Len(); i++)
+    //  printf("%d ", V[i].Val);
+    //printf("\n");
   }
+  printf("Total number of elements inserted: %d\n", sum);
   return 0;
 }
